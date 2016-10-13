df0d97c351c68f3f9c6d05db75db551192eeaffc
veremin <vladimir.eremin@veeam.com>
2016-10-13 17:49:16 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 GroupName=$1
+RouteTableName=$2
 touch /root/azurefile
-echo "'$GroupName'" > /root/azurefile
+echo "'$GroupName'" >> /root/azurefile
+echo "'$RouteTableName'" >> /root/azurefile
