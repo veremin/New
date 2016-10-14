afb4ff0892e2befb13ef98e67460a156fc12c25e
veremin <vladimir.eremin@veeam.com>
2016-10-14 18:30:07 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 GroupName=$1
 RouteTableName=$2
+SubnetPrefix=$3
 touch /root/azurefile
 echo "'$GroupName'" >> /root/azurefile
 echo "'$RouteTableName'" >> /root/azurefile
+echo "'SubnetPrefix'" >> /root/azurefile
