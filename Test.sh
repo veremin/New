5f7abcafa307fd278a5fea735685e59a23ec8efc
veremin <vladimir.eremin@veeam.com>
2016-10-19 20:39:31 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -2,7 +2,11 @@
 GroupName=$1
 RouteTableName=$2
 SubnetPrefix=$3
+ServiceAccountName=$4
+ServiceAccountPassword=$5
 touch /root/azurefile
 echo "'$GroupName'" >> /root/azurefile
 echo "'$RouteTableName'" >> /root/azurefile
 echo "'$SubnetPrefix'" >> /root/azurefile
+echo "'$ServiceAccountName'" >> /root/azurefile
+echo "'$ServiceAccountPassword'" >> /root/azurefile
