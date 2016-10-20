c0cf356ffaac503202c56102a0bca8a7fa83fb4e
veremin <vladimir.eremin@veeam.com>
2016-10-20 21:25:03 +0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -5,4 +5,4 @@ subnetAddr=$3
 accName=$4
 accPwd=$5
 touch /root/azurefile
-echo '{"resGroup":"$resGroup","routingTable":"$routingTable","subnetAddr":"$subnetAddr","accName":"$accName","accPwd":"accPwd"}' >> /root/azurefile
+echo "{"resGroup":"$resGroup","routingTable":"$routingTable","subnetAddr":"$subnetAddr","accName":"$accName","accPwd":"accPwd"}" >> /root/azurefile
