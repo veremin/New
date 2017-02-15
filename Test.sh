cae13f36fd0989a245a79723fc736d88eece32c9
veremin <vladimir.eremin@veeam.com>
2017-02-15 18:26:51 +0300

    Delete Test.sh

--- Test.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-resGroup=$1
-routingTable=$2
-subnetAddr=$3
-accName=$4
-accPwd=$5
-touch /root/azurefile
-echo '{"resGroup":"'$resGroup'","routingTable":"'$routingTable'","subnetAddr":"'$subnetAddr'","accName":"'$accName'","accPwd":"'$accPwd'"}' >> /root/azurefile
